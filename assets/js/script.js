956cb39ddf20fb058fe77c10ffe061c14ff6ce59
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-05-11 15:51:57 -0400

    add task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
@@ -9,4 +7,13 @@ var createTaskHandler = function() {
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }
-  buttonEl.addEventListener("click", createTaskHandler);
+  var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+  formEl.addEventListener("submit", createTaskHandler);
